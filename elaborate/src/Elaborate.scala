4c8ac39b4d7f7cbb88cfd53b0505bac3bf7f8223
ysyx_23060171_yzs <320016@qq.com>
2024-11-22 21:45:51 +0800

    use serilazableModule/elaborate

--- elaborate/src/Elaborate.scala
+++ elaborate/src/Elaborate.scala
@@ -1,10 +1,53 @@
-object Elaborate extends App {
-  val firtoolOptions = Array("--lowering-options=" + List(
-    // make yosys happy
-    // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
-    "disallowLocalVariables",
-    "disallowPackedArrays",
-    "locationInfoStyle=wrapInAtSquareBracket"
-  ).reduce(_ + "," + _))
-  circt.stage.ChiselStage.emitSystemVerilogFile(new rtl.GCD(), args, firtoolOptions)
+//object Elaborate extends App {
+//  val firtoolOptions = Array("--lowering-options=" + List(
+//    // make yosys happy
+//    // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
+//    "disallowLocalVariables",
+//    "disallowPackedArrays",
+//    "locationInfoStyle=wrapInAtSquareBracket"
+//  ).reduce(_ + "," + _))
+//  circt.stage.ChiselStage.emitSystemVerilogFile(new rtl.GCD(), args, firtoolOptions)
+//}
+package elaborate
+import mainargs._
+import rtl.{GCD, GCDParameter}
+
+object Elaborate extends SerializableModuleElaborator {
+  val topName = "GCD"
+
+  implicit object PathRead extends TokensReader.Simple[os.Path] {
+    def shortName = "path"
+    def read(strs: Seq[String]) = Right(os.Path(strs.head, os.pwd))
+  }
+
+  @main
+  case class GCDParameterMain(
+    @arg(name = "width") width: Int,
+    @arg(name = "useAsyncReset") useAsyncReset: Boolean) {
+    require(width > 0, "width must be a non-negative integer")
+    require(chisel3.util.isPow2(width), "width must be a power of 2")
+    def convert: GCDParameter = GCDParameter(width, useAsyncReset)
+  }
+
+  implicit def GCDParameterMainParser: ParserForClass[GCDParameterMain] =
+    ParserForClass[GCDParameterMain]
+
+  @main
+  def config(
+    @arg(name = "parameter") parameter:  GCDParameterMain,
+    @arg(name = "target-dir") targetDir: os.Path = os.pwd
+  ) =
+    os.write.over(targetDir / s"${topName}.json", configImpl(parameter.convert))
+
+  @main
+  def design(
+    @arg(name = "parameter") parameter:  os.Path,
+    @arg(name = "target-dir") targetDir: os.Path = os.pwd
+  ) = {
+    val (firrtl, annos) = designImpl[GCD, GCDParameter](os.read.stream(parameter))
+    os.write.over(targetDir / s"${topName}.fir", firrtl)
+    os.write.over(targetDir / s"${topName}.anno.json", annos)
+  }
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args.toIndexedSeq)
 }
--- rtl/src/GCD.scala
+++ rtl/src/GCD.scala
@@ -1,31 +1,81 @@
 package rtl
 
 import chisel3._
+import chisel3.experimental.hierarchy.{instantiable, public, Instance, Instantiate}
+import chisel3.experimental.{SerializableModule, SerializableModuleParameter}
+import chisel3.probe.{define, Probe, ProbeValue}
+import chisel3.properties.{AnyClassType, Class, Property}
+import chisel3.util.{DecoupledIO, Valid}
 
-/**
-  * Compute GCD using subtraction method.
-  * Subtracts the smaller from the larger until register y is zero.
-  * value in register x is then the GCD
-  */
-class GCD extends Module {
-  val io = IO(new Bundle {
-    val value1        = Input(UInt(16.W))
-    val value2        = Input(UInt(16.W))
-    val loadingValues = Input(Bool())
-    val outputGCD     = Output(UInt(16.W))
-    val outputValid   = Output(Bool())
-  })
+object GCDParameter {
+  implicit def rwP: upickle.default.ReadWriter[GCDParameter] =
+    upickle.default.macroRW
+}
 
-  val x = Reg(UInt())
-  val y = Reg(UInt())
+/** Parameter of [[GCD]] */
+case class GCDParameter(width: Int, useAsyncReset: Boolean) extends SerializableModuleParameter
+
+/** Verification IO of [[GCD]] */
+class GCDProbe(parameter: GCDParameter) extends Bundle {
+  val busy = Bool()
+}
+
+/** Metadata of [[GCD]]. */
+@instantiable
+class GCDOM(parameter: GCDParameter) extends Class {
+  val width:         Property[Int]     = IO(Output(Property[Int]()))
+  val useAsyncReset: Property[Boolean] = IO(Output(Property[Boolean]()))
+  width         := Property(parameter.width)
+  useAsyncReset := Property(parameter.useAsyncReset)
+}
+
+/** Interface of [[GCD]]. */
+class GCDInterface(parameter: GCDParameter) extends Bundle {
+  val clock  = Input(Clock())
+  val reset  = Input(if (parameter.useAsyncReset) AsyncReset() else Bool())
+  val input  = Flipped(DecoupledIO(new Bundle {
+    val x = UInt(parameter.width.W)
+    val y = UInt(parameter.width.W)
+  }))
+  val output = Valid(UInt(parameter.width.W))
+  val probe  = Output(Probe(new GCDProbe(parameter)))
+  val om     = Output(Property[AnyClassType]())
+}
+
+/** Hardware Implementation of GCD */
+@instantiable
+class GCD(val parameter: GCDParameter)
+    extends FixedIORawModule(new GCDInterface(parameter))
+    with SerializableModule[GCDParameter]
+    with ImplicitClock
+    with ImplicitReset {
+  override protected def implicitClock: Clock = io.clock
+  override protected def implicitReset: Reset = io.reset
+
+  val x: UInt = Reg(chiselTypeOf(io.input.bits.x))
+  // Block X-state propagation
+  val y: UInt = RegInit(chiselTypeOf(io.input.bits.x), 0.U)
+  val startupFlag = RegInit(false.B)
+  val busy        = y =/= 0.U
 
   when(x > y) { x := x - y }.otherwise { y := y - x }
 
-  when(io.loadingValues) {
-    x := io.value1
-    y := io.value2
+  when(io.input.fire) {
+    x           := io.input.bits.x
+    y           := io.input.bits.y
+    startupFlag := true.B
   }
 
-  io.outputGCD   := x
-  io.outputValid := y === 0.U
+  io.input.ready  := !busy
+  io.output.bits  := x
+  io.output.valid := startupFlag && !busy
+
+  // Assign Probe
+  val probeWire: GCDProbe = Wire(new GCDProbe(parameter))
+  define(io.probe, ProbeValue(probeWire))
+  probeWire.busy := busy
+
+  // Assign Metadata
+  val omInstance: Instance[GCDOM] = Instantiate(new GCDOM(parameter))
+  io.om := omInstance.getPropertyReference.asAnyClassType
 }
