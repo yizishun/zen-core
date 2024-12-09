5953b425d50920b736e176cea3f7dbf042c77578
ysyx_23060171_yzs <320016@qq.com>
2024-12-10 02:48:44 +0800

    generate mlir file during make verilog

--- scripts/elaborate.mk
+++ scripts/elaborate.mk
@@ -2,13 +2,16 @@
 RTL_DIR = $(BUILD_DIR)/rtl
 RTL_LIST = $(RTL_DIR)/filelist.f
 ELABORATE_DIR = $(RTL_DIR)/elaborate
+FIR_DIR = $(ELABORATE_DIR)/fir
+MLIR_DIR = $(ELABORATE_DIR)/mlir
+FIR_FILES = $(shell find $(abspath $(FIR_DIR)) -name "*.fir")
 #tb
 TB_RTL_DIR = $(CHISEL_TB_DIR)/build
 TB_RTL_LIST = $(TB_RTL_DIR)/filelist.f
 TB_ELABORATE_DIR = $(TB_RTL_DIR)/elaborate
-#fir files
-FIR_FILES = $(shell find $(abspath $(ELABORATE_DIR)) -name "*.fir")
-TB_FIR_FILES = $(shell find $(abspath $(TB_ELABORATE_DIR)) -name "*.fir")
+TB_FIR_DIR = $(TB_ELABORATE_DIR)/fir
+TB_MLIR_DIR = $(TB_ELABORATE_DIR)/mlir
+TB_FIR_FILES = $(shell find $(abspath $(TB_FIR_DIR)) -name "*.fir")
 #firtool options
 FIRTOOL_OPTION = \
 	-O=debug \
@@ -23,35 +26,37 @@ config:
 	mill -i elaborateRTL.runMain elaborate.Elaborate_$(DESIGN) config --width 32 --useAsyncReset true --target-dir config
 
 .PHONY: fir
-fir: config
-	mkdir -p $(ELABORATE_DIR)
-	mill -i elaborateRTL.runMain elaborate.Elaborate_$(DESIGN) design --target-dir $(ELABORATE_DIR) --parameter ./config/$(DESIGN).json
+fir:
+	mkdir -p $(FIR_DIR)
+	mill -i elaborateRTL.runMain elaborate.Elaborate_$(DESIGN) design --target-dir $(FIR_DIR) --parameter ./config/$(DESIGN).json
 
 .PHONY: verilog
 verilog: fir
-	mkdir -p $(RTL_DIR)
-	for file in $(FIR_FILES); do \
-		basename=$$(basename $$file .fir); \
-		anno_file=$(ELABORATE_DIR)/$$basename.anno.json; \
-		firtool $(FIRTOOL_OPTION) --annotation-file $$anno_file $$file -o $(RTL_DIR); \
-	done
+	$(call fir2rtl,$(FIR_DIR),$(FIR_FILES),$(MLIR_DIR),$(RTL_DIR))
 	find $(RTL_DIR) -maxdepth 1 -name "*.sv" -type f -print > $(RTL_LIST)
 
 .PHONY: tb-fir
 tb-fir:
-	mkdir -p $(TB_ELABORATE_DIR)
-	mill -i elaborateTB.runMain $(DESIGN)TestBenchMain design --target-dir $(TB_ELABORATE_DIR) --parameter ./config/$(DESIGN)TestBench.json
+	mkdir -p $(TB_FIR_DIR)
+	mill -i elaborateTB.runMain $(DESIGN)TestBenchMain design --target-dir $(TB_FIR_DIR) --parameter ./config/$(DESIGN)TestBench.json
 
 .PHONY: tb-verilog
 tb-verilog: tb-fir
-	mkdir -p $(TB_RTL_DIR)
-	for file in $(TB_FIR_FILES); do \
-    basename=$$(basename $$file .fir); \
-    anno_file=$(TB_ELABORATE_DIR)/$$basename.anno.json; \
-    firtool $(FIRTOOL_OPTION) --annotation-file $$anno_file $$file -o $(TB_RTL_DIR); \
-	done
+	$(call fir2rtl,$(TB_FIR_DIR),$(TB_FIR_FILES),$(TB_MLIR_DIR),$(TB_RTL_DIR))
 	find $(TB_RTL_DIR) -maxdepth 1 -name "*.sv" -type f -print > $(TB_RTL_LIST)
 
+define fir2rtl
+	mkdir -p $(3)
+	for file in $(2); do \
+    basename=$$(basename $$file .fir); \
+    anno_file=$(1)/$$basename.anno.json; \
+		firtool $(FIRTOOL_OPTION) \
+			--annotation-file $$anno_file \
+			$$file \
+			--output-final-mlir=$(3)/$$basename.mlir \
+			-o $(4); \
+	done
+endef
 
 help:
 	mill -i elaborateRTL.run --help
